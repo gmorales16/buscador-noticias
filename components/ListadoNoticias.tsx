3c83082018d25f892c4a827425f0cd23cd8af7d6
gmoales16 <guillermo.gazille@gmail.com>
2023-08-22 18:58:38 -0300

    Fix deploy errors

--- components/ListadoNoticias.tsx
+++ components/ListadoNoticias.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import useNoticias from "@/hooks/useNoticias";
--- context/NoticiasProvider.tsx
+++ context/NoticiasProvider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import axios from "axios";
 import React, { useState, useEffect, createContext, ReactNode } from "react";
 
